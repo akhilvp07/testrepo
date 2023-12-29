35d0ba6b19b54a9d29c1ff01827f02b868683ad2
Akhil Valsamrithan <avalsamrithan@rbbn.com>
2023-12-29 11:35:50 +0530

    Initial commit

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Printing the message to the console
+    printf("Hello ThinkPalm\n");
+
+    // Returning 0 to indicate successful execution
+    return 0;
+}
